d6de40b9c2ee180a02f862dcf2bd9c30e629476e
Puneet <47234953+PM74367@users.noreply.github.com>
2020-02-06 21:47:44 +0530

    Create linear_sort.c

---
+++ linear_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,n;
+  printf("Enter the no. of elements:");
+  scanf("%d",&n);
+  int *ar=(int *)malloc(sizeof(int)*n);
+  printf("Enter elements in array:\n");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&ar[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      if(ar[i]>ar[j])
+      {
+        ar[i]=ar[i]^ar[j];
+        ar[j]=ar[i]^ar[j];
+        ar[i]=ar[i]^ar[j];
+      }
+    }
+  }
+  printf("Array after sorting is\n");
+  for(i=0;i<n;i++)
+  {
+    printf("%d\t",ar[i]);
+  }
+  return 0;
+}

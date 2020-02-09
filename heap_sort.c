c49a38258fcfdf1f6e9bdd5481cef849ae7356e4
Puneet <47234953+PM74367@users.noreply.github.com>
2020-02-09 22:28:04 +0530

    Update heap_sort.c

--- heap_sort.c
+++ heap_sort.c
@@ -2,7 +2,24 @@
 #include<stdlib.h>
 void heap(int *ar,int st,int end)
 {
-//implement heap function
+  int elem=st;
+  int ch1=2*st+1;
+  int ch2=2*st+2;
+  if(ch1<end&&ar[elem]<ar[ch1])
+  {
+    elem=ch1;
+  }
+  if(ch2<end&&ar[elem]<ar[ch2])
+  {
+    elem=ch2;
+  }
+  if(elem!=st)
+  {
+    ar[elem]=ar[elem]^ar[st];
+    ar[st]=ar[elem]^ar[st];
+    ar[elem]=ar[elem]^ar[st];
+    heap(ar,elem,end);
+  }
 }
 int main()
 {
@@ -14,13 +31,19 @@ int main()
   {
     scanf("%d",&ar[i]);
   }
-  heap(ar,0,n);
-  for(i=n-1,i>0;i--)
+  for(i=n/2;i>=0;i--)
+    heap(ar,i,n);
+  for(i=n-1;i>0;i--)
   {
     int temp=ar[i];
     ar[i]=ar[0];
     ar[0]=temp;
     heap(ar,0,i);
+  }
+  printf("Array after sorting is\n");
+  for(i=0;i<n;i++)
+  {
+    printf("%d\t",ar[i]);
   }
   free(ar);
   return 0;
